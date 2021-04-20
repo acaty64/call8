6cb230a1e075b3d2415a2f1791ae12b100a04a35
Ana <ana.arashiro@gmail.com>
2021-04-20 10:23:56 -0500

    Phpunit test configuration

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+
+    public function testUser()
+    {
+        $this->assertTrue(true);
+        $this->assertDatabaseHas('users', [
+	        'name' => 'Master',
+            'email' => 'master@gmail.com',
+	    ]);
+    }
+}

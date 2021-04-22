81fa4a6673e98335cc8fe5d3c84f40faefe47a72
Ana <ana.arashiro@gmail.com>
2021-04-22 14:01:41 -0500

    Livewire Client Screen schema

--- app/Http/Livewire/ClientScreen.php
+++ app/Http/Livewire/ClientScreen.php
@@ -8,20 +8,28 @@ class ClientScreen extends Component
 {
 	public $qcalls = 10;
 	public $qwindows = 3;
-	public $status = "En Pausa";
+	public $status = "";
 
     public function render()
     {
         return view('livewire.client-screen');
     }
 
+    public function call()
+    {
+        $this->status = 'En Pausa';
+    	session()->flash('message', 'Poner en cola');
+    }
+
     public function connect()
     {
-    	session()->flash('message', 'Conectando ....');
+        $this->status = 'Atendiendo';
+        session()->flash('message', 'Conectando ....');
     }
 
     public function disconnect()
     {
+        $this->status = 'Cerrado';
     	session()->flash('message', 'Desconectando ....');
     }
 
--- resources/views/livewire/client-screen.blade.php
+++ resources/views/livewire/client-screen.blade.php
@@ -9,16 +9,22 @@
             </div>
         @endif
     </div>
-	<div>
-		Esperando: {{ $qcalls }}
+    <div class="row">
+		<div class=col-sm>
+			Estado: {{ $status }}
+		</div>
+		<div class=col-sm>
+			Esperando: {{ $qcalls }}
+		</div>
+		<div class=col-sm>
+			Atendiendo: {{ $qwindows }}
+		</div>
+    </div>
+	<div class="container" align="center">
+		<h1>Video-chat</h1>
 	</div>
 	<div>
-		Atendiendo: {{ $qwindows }}
-	</div>
-	<div>
-		Status: {{ $status }}
-	</div>
-	<div>
+		<button wire:click="call" class="btn btn-large btn-warning">Poner en Cola</button>
 		<button wire:click="connect" class="btn btn-large btn-success">Responder</button>
 		<button wire:click="disconnect" class="btn btn-large btn-danger">Colgar</button>
 	</div>

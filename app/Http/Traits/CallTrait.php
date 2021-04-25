6726c932876aa8cf20feba64cd34bbed7fd3da6e
Ana <ana.arashiro@gmail.com>
2021-04-25 14:28:43 -0500

    Client close the call

--- app/Http/Traits/CallTrait.php
+++ app/Http/Traits/CallTrait.php
@@ -6,11 +6,12 @@ use App\Models\Call;
 use App\Models\Status;
 use App\Models\Trace;
 use App\Models\User;
+use App\Models\Window;
 
 trait CallTrait
 {
 
-    public function store()
+    public function open()
     {
         $user = \Auth::user();
 
@@ -40,13 +41,18 @@ trait CallTrait
 
         $user = \Auth::user();
 
-        $status = Status::where('status', 'Cerrado')->first();
+        $status_closed = Status::where('status', 'Cerrado')->first();
+        $status_paused = Status::where('status', 'En Pausa')->first();
 
         $call = Call::findOrFail($array['id']);
-
-        $call->status_id = $status->id;
-
+        $call->status_id = $status_closed->id;
         $call->save();
+
+        $window = Window::where('client_id', $user->id)->first();
+        $window->status_id = $status_paused->id;
+        $window->client_id = null;
+        $window->call_id = null;
+        $window->save();
 
         $check = Trace::new_call($call);
 
--- tests/Feature/Trait_CallTest.php
+++ tests/Feature/Trait_CallTest.php
@@ -6,6 +6,7 @@ use App\Http\Traits\CallTrait;
 use App\Models\Call;
 use App\Models\Status;
 use App\Models\User;
+use App\Models\Window;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -16,7 +17,7 @@ class Trait_CallTest extends TestCase
     use DatabaseTransactions;
     use CallTrait;
 
-    public function test_store_a_new_call()
+    public function test_open_a_new_call()
     {
         $user = User::findOrFail(3);
 
@@ -24,7 +25,7 @@ class Trait_CallTest extends TestCase
 
         $status = Status::where('status', 'En Pausa')->first();
 
-        $response = $this->store();
+        $response = $this->open();
 
         $this->assertDatabaseHas('calls', [
             'user_id' => $user->id,
@@ -40,7 +41,8 @@ class Trait_CallTest extends TestCase
 
     }
 
-    public function test_close_a_call()
+
+    public function test_close_the_call()
     {
         $user = User::findOrFail(4);
 
@@ -48,7 +50,8 @@ class Trait_CallTest extends TestCase
 
         $this->actingAs($user);
 
-        $status = Status::where('status', 'Cerrado')->first();
+        $status_closed = Status::where('status', 'Cerrado')->first();
+        $status_paused = Status::where('status', 'En Pausa')->first();
 
         $array = [
             'id' => $call->id,
@@ -57,18 +60,27 @@ class Trait_CallTest extends TestCase
             'status_id' => $call->status_id,
         ];
 
+        $window = Window::where('client_id', $user->id)->first();
+
         $response = $this->close($array);
 
         $this->assertDatabaseHas('calls', [
             'user_id' => $user->id,
             'number' => $call->id,
-            'status_id' => $status->id,
+            'status_id' => $status_closed->id,
+        ]);
+
+        $this->assertDatabaseHas('windows', [
+            'id' => $window->id,
+            'client_id' => null,
+            'call_id' => null,
+            'status_id' => $status_paused->id,
         ]);
 
         $this->assertDatabaseHas('traces', [
             'user_id' => $user->id,
             'call_id' => $call->id,
-            'status_id' => $status->id
+            'status_id' => $status_closed->id
         ]);
 
     }

0d5184b1c7252e43d66a16e9c1d5e8ed6a519087
Ana <ana.arashiro@gmail.com>
2021-05-19 09:13:35 -0500

    Livewire test/

--- resources/views/tests/tests.blade.php
+++ resources/views/tests/tests.blade.php
@@ -23,15 +23,15 @@
                 </div>
                 <div class="card-body">
                     <h1>Prueba Livewire Channel</h1>
-                    @livewire('test1-screen')
+                    @livewire('test.test1-screen')
                 </div>
                 <div class="card-body">
                     <h1>Prueba Livewire Private Channel</h1>
-                    @livewire('test2-screen')
+                    @livewire('test.test2-screen')
                 </div>
                 <div class="card-body">
                     <h1>Prueba Livewire Presence Channel</h1>
-                    @livewire('test3-screen')
+                    @livewire('test.test3-screen')
                 </div>
                 <div class="card-body">
                     <h1>Prueba Channel</h1>

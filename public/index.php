618d389cf7b7b96ece3a32e66f93ad9ec465c315
Ana <ana.arashiro@gmail.com>
2021-05-29 19:49:47 -0500

    Http to Https

--- public/index.php
+++ public/index.php
@@ -48,8 +48,15 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
-$response = tap($kernel->handle(
-    $request = Request::capture()
-))->send();
+
+// Fuente: https://programacionymas.com/blog/cargar-assets-https-ssl-proyecto-laravel
+// $response = tap($kernel->handle(
+//     $request = Request::capture()
+// ))->send();
+
+$response = $kernel->handle(
+    $request = App\Custom\Http\Request::capture()
+);
+
 
 $kernel->terminate($request, $response);

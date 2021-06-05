997bdef8d5c7de37718e4ab3049ee869f5a9c51c
Ana <ana.arashiro@gmail.com>
2021-06-05 09:51:50 -0500

    public/index.php

--- public/index.php
+++ public/index.php
@@ -48,15 +48,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
-if(env('APP_ENV') === 'production'){
-	$response = tap($kernel->handle(
-	    $request = App\Custom\Http\Request::capture()
-	))->send();
-}else{
-	$response = tap($kernel->handle(
-	    $request = Request::capture()
-	))->send();
-}
-
+$response = tap($kernel->handle(
+    $request = Request::capture()
+))->send();
 
 $kernel->terminate($request, $response);
